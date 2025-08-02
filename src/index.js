8c245ca7a1267e033aadc2b0d58345fe3797fe27
timoheimonen <timo.heimonen@gmail.com>
2025-08-02 22:52:03 +0300

    fix: Security Headers(CSP) tuned.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const baseSecurityHeaders = {
   'X-Content-Type-Options': 'nosniff',
   'X-Frame-Options': 'DENY',
   'X-XSS-Protection': '1; mode=block',
-  'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://*.cloudflareinsights.com blob:; style-src 'self' 'unsafe-inline' blob:; img-src 'self' data: https://challenges.cloudflare.com https://*.cloudflareinsights.com blob:; frame-src https://challenges.cloudflare.com https://*.cloudflareinsights.com; connect-src 'self' https://challenges.cloudflare.com https://*.cloudflareinsights.com; worker-src 'self' blob:; object-src 'none'; base-uri 'self'; form-action 'self';",
+  'Content-Security-Policy': "default-src 'none'; script-src 'self' https://challenges.cloudflare.com; style-src 'self'; img-src 'self' https://challenges.cloudflare.com; frame-src https://challenges.cloudflare.com; connect-src 'self' https://challenges.cloudflare.com; worker-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';",
   'Referrer-Policy': 'strict-origin-when-cross-origin',
   'Permissions-Policy': 'geolocation=(), microphone=(), camera=(), payment=(), usb=(), magnetometer=(), gyroscope=()',
   'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
