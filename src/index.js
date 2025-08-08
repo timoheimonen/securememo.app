f8a29d3062bddc2640205c6eb2c6d869790bffe3
timoheimonen <timo.heimonen@gmail.com>
2025-08-08 13:37:19 +0300

    feat: add sitemap.xml

--- src/index.js
+++ src/index.js
@@ -182,6 +182,61 @@ export default {
         }
       }
 
+      // Serve sitemap.xml
+      if (pathname === '/sitemap.xml') {
+        if (request.method !== 'GET') {
+          return new Response(getErrorMessage('METHOD_NOT_ALLOWED'), {
+            status: 405,
+            headers: { 
+              'Allow': 'GET',
+              ...getSecurityHeaders(request)
+            }
+          });
+        }
+        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://securememo.app/</loc>
+    <lastmod>2025-01-01</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://securememo.app/about.html</loc>
+    <lastmod>2025-01-01</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://securememo.app/create-memo.html</loc>
+    <lastmod>2025-01-01</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://securememo.app/tos.html</loc>
+    <lastmod>2025-08-05</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+  <url>
+    <loc>https://securememo.app/privacy.html</loc>
+    <lastmod>2025-08-05</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.3</priority>
+  </url>
+</urlset>`;
+        return new Response(sitemap, {
+          headers: { 
+            'Content-Type': 'application/xml',
+            'Cache-Control': 'public, max-age=86400, s-maxage=86400, stale-while-revalidate=604800',
+            'ETag': '"sitemap-v1"',
+            'Last-Modified': 'Mon, 05 Aug 2025 00:00:00 GMT',
+            ...getSecurityHeaders(request)
+          }
+        });
+      }
+
       // Serve static assets
       if (pathname === '/styles.css') {
         if (request.method !== 'GET') {
