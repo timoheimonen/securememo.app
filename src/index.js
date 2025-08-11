80ddb6652e8fd1e94d8a29eff255c6c87356f445
timoheimonen <timo.heimonen@gmail.com>
2025-08-11 13:37:59 +0300

    feat: missing https://

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ import { getClientLocalizationJS } from './utils/clientLocalization.js';
 const allowedOrigins = [
   'https://securememo.app',
   'https://www.securememo.app',
-  'securememo-dev.timo-heimonen.workers.dev'
+  'https://securememo-dev.timo-heimonen.workers.dev'
 ];
 
 // Security headers with CSP for XSS protection (without CORS origin)
