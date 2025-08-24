833f755f2b6c14a84228af1343b0b596d0a35266
timoheimonen <timo.heimonen@gmail.com>
2025-08-24 19:19:17 +0300

    fix: update version

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ import {
 import { getClientLocalizationJS } from './utils/clientLocalization.js';
 
 // Immutable asset version for cache-busting (bump on asset changes)
-const ASSET_VERSION = '20250824d';
+const ASSET_VERSION = '20250824e';
 
 // Tiny, safe JS minifier for generated strings (removes comments and trims/collapses intra-line whitespace)
 function minifyJS(code) {
