1105c5bd5e571c969ebe7a4fa0c198d1d6137351
timoheimonen <timo.heimonen@gmail.com>
2025-08-26 15:04:58 +0300

    feat: color tests

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ import {
 import { getClientLocalizationJS } from './utils/clientLocalization.js';
 
 // Immutable asset version for cache-busting (bump on asset changes)
-const ASSET_VERSION = '20250826a';
+const ASSET_VERSION = '20250826b';
 
 // Tiny, safe JS minifier for generated strings (removes comments and trims/collapses intra-line whitespace)
 function minifyJS(code) {
