912b391ecf776427c0c2a239f6974fd654f94071
timoheimonen <timo.heimonen@gmail.com>
2025-08-29 19:05:22 +0300

    fix: regex

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -83,8 +83,8 @@ function encodeHtmlEntities(input) {
 export function normalizeCiphertextForResponse(input) {
   if (typeof input !== 'string') return '';
   // Remove null byte and other non-printable control chars except \n, \r, \t
-  // Use literal regex to avoid RegExp constructor with non-literal value
-  const controlRegex = /[\0\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x7F]/g;
+  // Use RegExp constructor to avoid ESLint error with control characters in regex literal
+  const controlRegex = new RegExp('[\\0\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\x0B\\x0C\\x0E\\x0F\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1A\\x1B\\x1C\\x1D\\x1E\\x1F\\x7F]', 'g');
   return input.replace(controlRegex, '');
 }
 
