7d2f68253a250f785fc5725d4c107154c369c66a
timoheimonen <timo.heimonen@gmail.com>
2025-08-29 19:17:26 +0300

    fix: regex

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -82,10 +82,10 @@ function encodeHtmlEntities(input) {
  */
 export function normalizeCiphertextForResponse(input) {
   if (typeof input !== 'string') return '';
-  // Remove null byte and other non-printable control chars except \n, \r, \t
-  // Use RegExp constructor to avoid ESLint error with control characters in regex literal
-  const controlRegex = new RegExp('[\\0\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\x0B\\x0C\\x0E\\x0F\\x10\\x11\\x12\\x13\\x14\\x15\\x16\\x17\\x18\\x19\\x1A\\x1B\\x1C\\x1D\\x1E\\x1F\\x7F]', 'g');
-  return input.replace(controlRegex, '');
+  // Remove null bytes first (explicit \0) then strip other disallowed control characters
+  // using the existing, well-audited helper which avoids regex literals with control escapes.
+  const withoutNull = input.replace(/\0/g, '');
+  return stripDisallowedControlChars(withoutNull);
 }
 
 /**
